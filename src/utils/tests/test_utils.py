5a3e3f4a6ce2d6c50a310640b96b69b0955e8e41
davdonin <dmitrii.avdonin@fitchsolutions.com>
2019-02-21 11:31:04 -0500

    updated readme and started writing tests for utils

---
+++ src/utils/tests/test_utils.py
@@ -0,0 +1,14 @@
+import unittest
+from mock import patch
+import mock
+from utils import toNpArray, saveNpArrayToFile, loadNpArrayFromFile
+
+
+def generateArrayData():
+    pass
+
+class TestUtilsMethods(unittest.TestCase):
+
+    def test_checkSavingAndLoadingData(self):
+        pass
+
