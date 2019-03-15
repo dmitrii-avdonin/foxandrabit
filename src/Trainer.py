fixed some bugs, updated label calculation, optimized reinforcement, played with settings

src/Trainer.py
@@ -33,25 +33,24 @@ def cnn_model_fn(features, labels, mode):
         inputs=input_layer,
         filters=64,
         kernel_size=[2, 2],
+        padding="valid",
+        activation=tf.nn.relu)
+
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=1)
+
+
+    # Convolutional Layer #2
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=32,
+        kernel_size=[2, 2],
         padding="same",
         activation=tf.nn.relu)
 
-    #pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    #pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
 
-
-    # # Convolutional Layer #2
-    # conv2 = tf.layers.conv2d(
-    #     inputs=pool1,
-    #     filters=16,
-    #     kernel_size=[5, 5],
-    #     padding="same",
-    #     activation=tf.nn.relu)
-
-    # pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-
-
-    pool1_flat = tf.reshape(conv1, [-1, 9* 9 * 64])
+    pool1_flat = tf.reshape(conv2, [-1, 7 * 7 * 32 ])
 
     # Dense Layer Densely connected layer
     dense1 = tf.layers.dense(inputs=pool1_flat, units=1024, activation=tf.nn.relu)
@@ -78,6 +77,7 @@ def cnn_model_fn(features, labels, mode):
     # Calculate Loss (for both TRAIN and EVAL modes)
     #loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
     loss = tf.reduce_mean(tf.abs(labels-logits))
+    #loss = tf.reduce_max(tf.abs(labels-logits))
 
     # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:

src/actions/reinforcement.py
@@ -23,12 +23,12 @@ class VarStore:
 def parseArgs(args):    
     argsCount = len(args)
 
-    targetStepsCount = 400    
+    targetStepsCount = 50
     if(argsCount>0):
         targetStepsCount = int(args[0])
     
-    width = 6
-    height = 6
+    width = 150
+    height = 150
     if(argsCount>=3):
         width = int(args[1])
         height = int(args[2])
@@ -45,7 +45,7 @@ def parseArgs(args):
 def addItemsToLib(data, lib, offset, threadIdx):
     i = threadIdx
     while i< len(data):
-        lib[hash(data[i].data.tobytes())] = i
+        lib[hash(data[i][:9, :, :].data.tobytes())] = i
         i += offset
         #if threadIdx==0 and i%100==0:
         #    print(str(len(lib)))
@@ -102,7 +102,7 @@ def reinforcement(args):
     field = Field(width, height, countR, countF, Mode.Reinforcement, seed = seed)
 
     decrees = 1.1   # the rate increment is decreasing if we go one step back
-    increment = 0.1 # the amout by wich we encrease the label
+    increment = 0.15 # the amout by wich we encrease the label
 
     vWeight = np.zeros(vr)
     inc = increment
@@ -157,7 +157,7 @@ def reinforcement(args):
                 for j in range(store.agentsCount):
                     if(not labelDirectionDelta[i][j].any()):  # ignoring if direction delta is zero, can happen for dead agents
                         continue
-                    stateHash = hash(store.dataQ[i][j].data.tobytes())
+                    stateHash = hash(store.dataQ[i][j][:9, :, :].data.tobytes())
                     if(not (stateHash in store.dataLib)):
                         store.labelQ[i][j] += labelDirectionDelta[i][j] 
                         store.trainData.append(store.dataQ[i][j])
@@ -190,7 +190,7 @@ def reinforcement(args):
         print("Remaining        = " + str(targetStepsCount-stepsCount))
         print("-----------------------------------------")
 
-        if(len(trainLabelsR)>1000):
+        if(len(trainLabelsR)>100000):
             for i in reversed(range(len(trainLabelsR))):
                 if(trainLabelsR[i][2].sum()==0):
                     del trainDataR[i]

src/field/Field.py
@@ -48,6 +48,8 @@ class MyMultiGrid(MultiGrid):
         self._place_agent(coords, agent)        
 
 class Field(Model):
+    wallConst = -9
+
     def __init__(self, width, height, num_rabits, num_foxes, mode, seed=None):
         self.mode = mode
         self.running = True
@@ -69,7 +71,7 @@ class Field(Model):
         Fox.DeadCount = 0
         self.datacollector = DataCollector(
         model_reporters={"RabitsNr": lambda model : model.aliveRabitsCount(), "FoxesNr": lambda model : model.aliveFoxesCount()},  # A function to call
-        agent_reporters={})  # An agent attribute
+        agent_reporters={})  # An agent attribute        
 
         self.rabits = []
         for i in range(self.num_rabits):
@@ -110,7 +112,7 @@ class Field(Model):
                     for j in range(vr*2+1):
                         (x,y) = nbh[i][j]
                         if self.grid.out_of_bounds((x, y)):  #this cell can't be reached 
-                            agentState[i][j] = [-1, -1, a.fullness]
+                            agentState[i][j] = [Field.wallConst, Field.wallConst, Field.wallConst]
                         else:
                             food = self.cells[x][y].food
                             food = round(food, 1) #rounding to one decimal place
@@ -201,23 +203,28 @@ class Field(Model):
                             for cy in self.getShift(dy, 1): # it is expected that vr is at leas mr+1 otherwise we can get out of range
                                 cell = state[cx][cy]
                                 if(cx==dx and cy==dy): #this is the direction cel
-                                    if(cell[2] > 0 ): # number of AgentType.Fox on this cell
-                                        lbl += -4
-
-                                    if((cx!=vr or cy!=vr)): # condition to avoid that the agent (whose state we analize) adds a minus to his current location
-                                        lbl += -0.4 * cell[1]
-
-                                    if(cell[1]==-1):  # a wall, cannot go there
+                                    if(cell[1]==Field.wallConst):  # a wall, cannot go there
                                         lbl += -2
                                     else:
                                         lbl += cell[0]
+
+                                        if(cell[2] > 0 ): # number of AgentType.Fox on this cell
+                                            lbl += -4
+
+                                        if(not(cx==vr and cy==vr)): # condition to avoid that the agent (whose state we analize) adds a minus to his possible next location
+                                            lbl += -0.4 * cell[1]
+
                                 else:
-                                    if(cell[2]>0):
-                                        lbl -= 2
-                                    if(cell[1]==-1):  # a wall, cannot go there
+                                    if(cell[1]==Field.wallConst):  # a wall, cannot go there
                                         lbl += -1
                                     else:
                                         lbl += cell[0] * 0.1
+
+                                        if(not(cx==vr and cy==vr)): # condition to avoid that the agent (whose state we analize) adds a minus to his possible next location
+                                            lbl += -0.2 * cell[1]
+
+                                        if(cell[2]>0):
+                                            lbl += -2
                                 
                         label[dx-stl][dy-stl] = lbl
 
@@ -252,17 +259,22 @@ class Field(Model):
                             for cy in self.getShift(dy, 1): # it is expected that vr is at leas mr+1 otherwise we can get out of range
                                 cell = state[cx][cy]
                                 if(cx==dx and cy==dy): #this is the direction cel
-                                    
-                                    lbl += 2 * cell[1] # bonus multiplied by number of rabits on that cell
+                                    if(cell[1]==Field.wallConst):  # a wall, cannot go there
+                                        lbl += -2
+                                    else:                                    
+                                        lbl += 2 * cell[1] # bonus multiplied by number of rabits on that cell
 
-                                    if((cx!=vr or cy!=vr)): # condition to avoid that the agent (whose state we analize) adds a minus to his current location
-                                        lbl += -0.2 * cell[2]
-                                    if(cell[1]==-1):  # a wall, cannot go there
-                                        lbl += -1
+                                        if(not(cx==vr and cy==vr)): # condition to avoid that the agent (whose state we analize) adds a minus to his possible next location
+                                            lbl += -0.4 * cell[2]
                                 else:
-                                    lbl += 1 * cell[1]
-                                    if(cell[1]==-1):  # a wall, cannot go there
-                                        lbl += -0.5                                    
+                                    if(cell[1]==Field.wallConst):  # a wall, cannot go there
+                                        lbl += -1
+                                    else:
+                                        lbl += 1 * cell[1]
+
+                                        if(not(cx==vr and cy==vr)): # condition to avoid that the agent (whose state we analize) adds a minus to his possible next location
+                                            lbl += -0.2 * cell[2]
+
                                 
                         label[dx-stl][dy-stl] = lbl
 
@@ -358,6 +370,12 @@ class Field(Model):
 
             #if self.mode==Mode.Training or self.mode==Mode.DataGeneration or self.mode==Mode.Reinforcement: # get labels for foxes
             labels = self.getLablesF(data)
+            # calcMoves = [np.argmax(l) for l in labels]
+            # errCounter = 0
+            # for k in range(len(moves)):
+            #     if(moves[k] != calcMoves[k]):
+            #         if(labels[k][calcMoves[k]] != labels[k][moves[k]]):
+            #             errCounter += 1
             
             self.setNextPos(self.foxes, moves)        
             self.scheduleFox.step()

src/utils/tSneSample.py
@@ -29,10 +29,6 @@ count = labelR[:, 2, :]
 def analyzeRabitsData():
     plt.figure(1)
 
-    updatesCount(plt)
-
-    updateSize(plt)
-
     tSNE(plt)
 
     plt.gca().yaxis.set_minor_formatter(NullFormatter())
@@ -46,7 +42,7 @@ def plot_embedding(X, plt, title=None):
     x_min, x_max = np.min(X, 0), np.max(X, 0)
     X = (X - x_min) / (x_max - x_min)     
     #plt.figure()
-    ax = plt.subplot(223)
+    ax = plt.subplot(111)
     for i in range(X.shape[0]):
         plt.text(X[i, 0], X[i, 1], "o",
                  color=plt.cm.Set1(np.argmax(labels[i]) / 10.),
@@ -75,41 +71,6 @@ def tSNE(plt):
         X_tsne = tsne.fit_transform(labels)
         plot_embedding(X_tsne, plt,
                 "t-SNE embedding of the labels (time %.2fs)" % (time() - t0))
-
-def updatesCount(plt):
-    # Reinforcement eficiency investigation
-    unique, counts = np.unique(np.array([a.sum() for a in count]), return_counts=True)
-
-    plt.subplot(221)
-    plt.bar(range(len(unique)), counts, tick_label=unique.astype(int))
-    plt.yscale('log')
-    plt.title('Update Count')
-    plt.xlabel('Number of times label was updated')
-    plt.ylabel('Number of Labels')
-    plt.grid(True)    
-
-    for i, v in enumerate(counts):
-        plt.text(i , 1.5 , str(v), color='black', verticalalignment='bottom', rotation=90)
-
-
-def updateSize(plt):
-    avgAbsDelta =  np.array([ np.sum(np.abs(a))/len(a) for a in delta if np.any(a)])
-    bins =  [i * 0.005 for i in range(41)]
-    digitized = np.digitize(avgAbsDelta, bins)
-    unique, counts = np.unique(digitized, return_counts=True)
-    labels =  ["<" + str(bins[u]) for u in unique]
-
-    plt.subplot(222)
-    plt.bar(range(len(unique)), counts, tick_label=labels)
-    plt.yscale('log')
-    plt.title('Update Amount')
-    plt.xlabel('Label average absolute update amount ')
-    plt.ylabel('Number of Labels')
-    plt.grid(True)
-
-    for i, v in enumerate(counts):
-        plt.text(i , 3.5 , str(v), color='black', verticalalignment='bottom', rotation=90)
-
 
 
 if __name__ == "__main__":